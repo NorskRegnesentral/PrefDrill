656f5e35c99e9877e04ae1f827898a670058384e
paigejo <paigejo@gmail.com>
2025-10-22 11:01:24 +0200

    excluded unnecessary pairs from figs refresh

--- R/simStudy.R
+++ R/simStudy.R
@@ -3144,7 +3144,7 @@ refreshManuscriptFigDir = function() {
   
   copyDirFiltered(srcDir=paste0("figures/simStudy/"), 
                   dstDir=paste0("~/fig/simStudy/"), 
-                  includeSubstr = c("agg", "par", "mean.*Coverage95"), excludeSubstr = c("80", "_Var", "RMSE", "prefPairs"))
+                  includeSubstr = c("agg", "par", "mean.*Coverage95"), excludeSubstr = c("80", "_Var", "RMSE", "Pairs"))
   
   copyDirFiltered(srcDir=paste0("figures/simStudy/"), 
                   dstDir=paste0("~/fig/simStudy/"), 
